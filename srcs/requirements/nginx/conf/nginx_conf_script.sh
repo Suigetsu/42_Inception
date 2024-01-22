1d861a4ce0fa81a2b9bc1d8c15a2ead5dfc02408
Suigetsu <lagmed.019@gmail.com>
2024-01-22 15:12:50 +0100

    experimenting with some scripts

---
+++ srcs/requirements/nginx/conf/nginx_conf_script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cat << EOF > /etc/nginx/http.d/nginx.conf
+server {
+    listen      443 ssl;
+    server_name  ${DOMAIN_NAME} www.${DOMAIN_NAME};
+    root    /var/www/;
+    index index.php index.html;
+    ssl_certificate     ${SSL_CERT};
+    ssl_certificate_key ${SSL_KEY};
+    ssl_protocols       TLSv1.2 TLSv1.3;
+    location / {
+        try_files ${DOLLAR_SIGN}uri /index.php?${DOLLAR_SIGN}args /index.html;
+        # add_header Last-Modified ${DOLLAR_SIGN}date_gmt;
+        add_header Cache-Control 'no-store, no-cache';
+        # if_modified_since off;
+        # expires off;
+        # etag off;
+    }
+}
+EOF
+nginx -g 'daemon off;'
