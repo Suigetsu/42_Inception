cf36b7a66376148166b0a7dff6aff1117f394aa0
Suigetsu <lagmed.019@gmail.com>
2024-02-01 17:41:05 +0100

    done

--- srcs/requirements/mariadb/conf/mysql_install_db.sh
+++ srcs/requirements/mariadb/conf/mysql_install_db.sh
@@ -3,11 +3,5 @@
 mkdir /var/run/mysqld
 chmod 777 /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
-cat << EOF > /etc/my.cnf.d/docker.cnf
-[mysqld]
-skip-host-cache
-skip-name-resolve
-bind-address=0.0.0.0
-EOF
-sed -i "s@skip-networking@skip-networking=0@" /etc/my.cnf.d/mariadb-server.cnf
+sed -i -e "s@skip-networking@skip-networking=0@" -e "s@#bind-address=0.0.0.0@bind-address=0.0.0.0@" /etc/my.cnf.d/mariadb-server.cnf
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
